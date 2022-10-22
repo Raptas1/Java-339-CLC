c73ca192465165493ebf2f5c906151b8d2be883a
Quan Nguyen <56704320+nlpquan@users.noreply.github.com>
2022-10-22 14:26:45 -0700

    Add files via upload

---
+++ LoginController.java
@@ -0,0 +1,33 @@
+package com.gcu.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gcu.model.UserModel;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+	@GetMapping("")
+	public String displayLogin(Model model) {
+		model.addAttribute("userModel", new UserModel());
+		return "login";
+	}
+	
+	@PostMapping("")
+	public String doLogin(@Valid UserModel userModel, BindingResult bindingResult, Model model) {
+		
+		if(bindingResult.hasFieldErrors("credentials.username") || bindingResult.hasFieldErrors("credentials.password")) {
+			return "/login";
+		}
+		
+		return ("redirect:/dashboard");
+	}
+}
