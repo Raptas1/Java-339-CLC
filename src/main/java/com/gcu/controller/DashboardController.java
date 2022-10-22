26d1c9036bc3095b6de9719a24ceea669b1e96ab
Quan Nguyen <56704320+nlpquan@users.noreply.github.com>
2022-10-22 14:28:19 -0700

    Add files via upload

--- src/main/java/com/gcu/controller/DashboardController.java
+++ src/main/java/com/gcu/controller/DashboardController.java
@@ -1,18 +1,27 @@
 package com.gcu.controller;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.gcu.model.ListingModel;
 import com.gcu.model.UserModel;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
-	
-	
+
 	@Autowired
 	UserModel userModel;
 
@@ -24,14 +33,36 @@ public class DashboardController {
 		model.addAttribute("totalRevenue", userModel.getTotalRevenue());
 		return "dashboard";
 	}
-	
+
 	@GetMapping("/myListings")
 	public String displayMyListings(Model model) {
+		List<ListingModel> listings = new ArrayList<ListingModel>();
+		model.addAttribute("myListings", listings);
 		return "myListings";
 	}
-	
+
 	@GetMapping("/listings")
 	public String displayListings(Model model) {
 		return "listings";
 	}
+
+	@GetMapping("/create")
+	public String create(Model model)
+	{
+		model.addAttribute("listingModel", new ListingModel());
+		return "create";
+	}
+
+	@PostMapping("/createSuccess")
+	public String createSuccess(@Valid ListingModel listingModel, BindingResult bindingResult, Model model)
+	{
+		if(bindingResult.hasErrors()) {
+            return "create";
+        }
+	    
+		model.addAttribute("listings", listingModel.generateListings(listingModel));
+
+		return "myListings";
+	}
+
 }
