8c0558b36e64760b1a7564b9bc320c93f72f4422
Ivan Gudino <Igudino2002@gmail.com>
2022-11-01 14:22:51 -0700

    Created UserDataService.java
    UserDataService includes methods for database fetching
    
    DataAccessInterface got two new methods that find by username/email

--- src/main/java/com/gcu/data/DataAccessInterface.java
+++ src/main/java/com/gcu/data/DataAccessInterface.java
@@ -5,7 +5,8 @@ import java.util.List;
 public interface DataAccessInterface <T> {
 
 	public List<T> findAll();
-	public T findById(int id);
+	public T findByUsername(String username);
+	public T findByEmail(String email);
 	public boolean create(T t);
 	public boolean update(T t);
 	public boolean delete(T t);
---
+++ src/main/java/com/gcu/data/UsersDataService.java
@@ -0,0 +1,62 @@
+package com.gcu.data;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gcu.data.entity.UsersEntity;
+import com.gcu.data.repository.UsersRepository;
+
+@Service
+public class UsersDataService implements DataAccessInterface<UsersEntity> {
+	
+	@Autowired
+	private UsersRepository usersRepository;
+	
+	public UsersDataService(UsersRepository usersRepository) {
+		this.usersRepository = usersRepository;
+	}
+
+	@Override
+	public List<UsersEntity> findAll() {
+		return null;
+	}
+
+	@Override
+	public boolean create(UsersEntity user) {
+		try {
+			this.usersRepository.save(user);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean update(UsersEntity user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(UsersEntity user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public UsersEntity findByUsername(String username) {
+		UsersEntity usersEntity = usersRepository.findByUsername(username);
+		return usersEntity;
+	}
+
+	@Override
+	public UsersEntity findByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
