5ef381478224c96ef04083cfe9944d35fd754d2c
Ivan Gudino <Igudino2002@gmail.com>
2022-11-01 14:26:43 -0700

    Remove Unnecesary Annotation
    Removed Annotation that caused errors.

--- src/main/java/com/gcu/MilestoneApplication.java
+++ src/main/java/com/gcu/MilestoneApplication.java
@@ -2,12 +2,11 @@ package com.gcu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 
+@SpringBootApplication()
 @ComponentScan({"com.gcu"})
-@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
 
 public class MilestoneApplication {
 
